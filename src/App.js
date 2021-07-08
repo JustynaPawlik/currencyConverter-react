d3bd10e85c9e7832dab69a1363a89993051aaec6
Justyna Pawlik <juspaw11@wp.pl>
2021-07-08 07:18:13 +0200

    delete unnecessary div

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
-        <div>
           <Form />
-        </div>
       </Container>
     </ThemeProvider>
   );
